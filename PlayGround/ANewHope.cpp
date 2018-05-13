ed4bbbcfde595f117c637325fa53960002df55e9
nhoughto <nhoughto5@gmail.com>
2018-05-13 12:03:54 -0700

    Removed garbade

--- PlayGround/ANewHope.cpp
+++
@@ -1,8 +0,0 @@
-#include "stdafx.h"
-#include "ANewHope.h"
-
-
-ANewHope::ANewHope() {}
-
-
-ANewHope::~ANewHope() {}
--- PlayGround/ANewHope.h
+++
@@ -1,8 +0,0 @@
-#pragma once
-class ANewHope
-{
-public:
-	ANewHope();
-	~ANewHope();
-};
-
